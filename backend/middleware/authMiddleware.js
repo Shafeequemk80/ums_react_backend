c90f2d0b6986eee24c7c1ad92e537fe35e031ebd
Shafeequemk80 <shafeequemk80@gmail.com>
2024-09-13 11:12:47 +0530

    cors issue httponly deleted

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,6 +5,7 @@ import User from "../models/userModels.js";
 const protect = asyncHandler(async (req, res, next) => {
   let token;
   token = req.cookies.Jwt;
+
   if (token) {
     try {
       const decoded = Jwt.verify(token, process.env.JWT_SECRET);
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,10 +7,9 @@ const generateToken=(res,userId)=>{
     })
    
  if (token) {
-    console.log(token);
-    
+   console.log(token);
+   
     res.cookie('Jwt',token,{
-        httpOnly:true,
         secure:process.env.NODE_ENV !=="development",
         sameSite:'none',
         maxAge:30 * 24 * 60 * 60 * 1000
